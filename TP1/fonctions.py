c196a5f01ca281102570fb9a6ecff56761fc07c4
damermic <mickael.damerval@univ-rouen.fr>
2025-09-04 14:29:26 +0200

    changeent de la fonction puissance par une boucle for

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,13 +3,18 @@ def puissance(a,b):
 		raise TypeError("Only integers are allowed")
 	if a == 0 and b < 0:
 		raise Exception("Puissance indéfinie pour 0 exposant négatif")
+	
 	if b > 0:
 		res = 1
 		for i in range(b):
 			res *= a
 		return res
+	
 	if b < 0:
 		res = 1
 		for i in range(-b):
 			res *= a
 		return 1/res
+	
+	if b == 0:
+		return 1
