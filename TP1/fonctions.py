2893b8d7d53cdb40f3e9b99f4d65c5e488ace258
damermic <mickael.damerval@univ-rouen.fr>
2025-09-04 14:14:53 +0200

    ajout des fonction cas limite

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,5 +1,7 @@
 def puissance(a,b):
 	if not type(a) is int or not type(b) is int:
 		raise TypeError("Only integers are allowed")
+	if a == 0 and b < 0:
+		raise Exception("Puissance indéfinie pour 0 exposant négatif")
 	return a**b
 
