fe0ce49b8ad379bc48aa098894921340fb80773b
damermic <mickael.damerval@univ-rouen.fr>
2025-09-05 14:07:43 +0200

    changement de la fonction puissance et du fichier test, ajout affichage de la branch et du repo

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,8 +1,15 @@
 def puissance(a,b):
+	
+	if a == 0 and b <= 0:
+		raise Exception("Puissance indéfinie pour 0 exposant négatif")
+	if a == 0 and b == 0:
+		raise Exception("0 puissance 0 est indéfinie")
+	
+	if type(a) is float or type(b) is float:
+		return a**b
+	
 	if not type(a) is int or not type(b) is int:
 		raise TypeError("Only integers are allowed")
-	if a == 0 and b < 0:
-		raise Exception("Puissance indéfinie pour 0 exposant négatif")
 	
 	if b > 0:
 		res = 1
--- TP1/test.py
+++ TP1/test.py
@@ -13,11 +13,15 @@ def test_2():
 	assert f.puissance(-2,-1) == -0.5
 
 def test_3():
-    assert f.puissance(0, 5) == 0
-    assert f.puissance(0, 100) == 0
+	assert f.puissance(0, 5) == 0
+	assert f.puissance(0, 2) == 0
+	assert f.puissance(0, 100) == 0
+	assert f.puissance(2.9,3.1) == 27.12900049510629
 
 def test_4():
-    with pytest.raises(Exception):
-        f.puissance(0, -1)
-    with pytest.raises(Exception):
-        f.puissance(0, -5)
+	with pytest.raises(Exception):
+		f.puissance(0, -1)
+	with pytest.raises(Exception):
+		f.puissance(0, -5)
+	with pytest.raises(Exception):
+		f.puissance(0, 0)
